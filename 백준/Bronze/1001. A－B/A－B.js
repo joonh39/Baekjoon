a1184bdb8e6dc4be0953f82a2cd963be31acbc57
joonh39 <138421910+joonh39@users.noreply.github.com>
2023-07-03 22:48:36 +0900

    [Bronze V] Title: A-B, Time: 120 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const inputData = fs.readFileSync('/dev/stdin').toString().split(' ');
+let A = parseInt(inputData[0]);
+let B = parseInt(inputData[1]);
+console.log (A - B);
