4b62308bbe671cb4d1e5a02e38688699cee0e04b
joonh39 <138421910+joonh39@users.noreply.github.com>
2023-07-03 22:51:42 +0900

    [Bronze V] Title: 사칙연산, Time: 124 ms, Memory: 9612 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const inputData = fs.readFileSync('/dev/stdin').toString().split(' ');
+let A = parseInt(inputData[0]);
+let B = parseInt(inputData[1]);
+console.log (A + B);
+console.log (A - B);
+console.log (A * B);
+console.log (parseInt(A / B));
+console.log (A % B);
