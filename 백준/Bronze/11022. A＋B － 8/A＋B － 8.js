b9d37602de0c290e1bb5b9f35532af076be3c126
joonh39 <138421910+joonh39@users.noreply.github.com>
2023-07-03 23:34:01 +0900

    [Bronze V] Title: A+B - 8, Time: 124 ms, Memory: 9532 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const inputData = fs.readFileSync('/dev/stdin').toString().split('\n');
+const T = Number(inputData[0]);
+for (let i = 1; i <= T; i++) {
+    let tCase = inputData[i].split(' ');
+    let [A, B] = [Number(tCase[0]), Number(tCase[1])];
+    console.log (`Case #${i}: ${A} + ${B} = ${A + B}`);
+}
