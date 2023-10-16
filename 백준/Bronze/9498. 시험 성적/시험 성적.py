57b804e2ca343b5bd5eec956eed5d7962256d35b
joonh39 <138421910+joonh39@users.noreply.github.com>
2023-10-16 13:43:29 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,10 @@
+score = int(input())
+result = ''
+
+if score >= 100 or score >= 90 : result = 'A'
+elif score >= 80 : result = 'B'
+elif score >= 70 : result = 'C'
+elif score >= 60 : result = 'D'
+else : result = 'F'
+
+print(result)
